989a341a6f4cfe71a5841b6268cf03a4ad7940b9
Brennan <hic11001@byui.edu>
2020-08-17 18:03:54 -0600

    some changes

--- home.php
+++ home.php
@@ -11,17 +11,28 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <h1>Lindy's Corner</h1>
+        <header>
+        <h1 class="title">Lindy's Corner</h1>
 		<nav>
 			<ul>
                 <a href="#"><li>Home</li></a>
 				<a href="#"><li>About Me</li></a>
 			</ul>
         </nav>
-        <h2>Be the Best You Can Be</h2>
+        <hr>
+        </header>
+        <h2 class="bookTitle">Be the Best You Can Be</h2>
         <a href="#"><h3>Purchase Here</h3></a>
-        <img src="./mcorner/pics/small_book_pic.jpg" alt="Front Cover of Book">
-        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        <img src="pics/small_book_pic.jpg" alt="Front Cover of Book">
+
+        <p class="profession">Author and Illustrator</p>
+        <h2 class="authorName">Melinda B. Hicks</h2>
+        <div class="text">
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p></div>
+        <hr>
+        <footer>
+            <h1>Social Media</h1>
+        </footer>
     </body>
 
 </html>
